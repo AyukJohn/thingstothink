2634c21e10aeba887a798459d4167fe0f28f545f
johnayuk <john1234ayuk@gmail.com>
2023-11-26 18:29:22 +0100

    did some code on the users page

--- src/components/DashBoard/usersList.vue
+++ src/components/DashBoard/usersList.vue
@@ -58,53 +58,68 @@
 
                 <div class="bg-light container mt-5"  style="width: 95%; height: 64vh !important; overflow-y: auto;">
 
-                    <div class="container" style="width: 88%;">
+                    <div  v-for="user in userList" :key="user.id" class="container" style="width: 88%;">
 
                         <div class="row mt-5 mb-5">
+                        
 
                             <div class="col-md-3">
-
+                                <div class="user-avatar">
+                                    <img :src="user.image" alt="image here">
+                                </div>
                             </div>
 
                             <div class="col-md-3">
-                                
+                                <h5 style="margin-top: 5%;">{{ user.first_name }}</h5> 
                             </div>
 
+                            <div class="col-md-3"> 
+                                <div v-if="user.verified === 'Yes'" class="d-flex align-items-center">
+                                    <img class="img2" src="@/assets/verified.svg" alt="Verified Icon">
+                                    <h6 class="mt-2">Verified</h6>
+                                </div>
+                            </div>
+                            
                             <div class="col-md-3">
-                                
+                                <button class="btn  btn-small text-light" style=" width: 120px; margin-top: 5%; background-color: #D6A12B">View All</button>
                             </div>
 
-                            <div class="col-md-3">
-                                
-                            </div>
-
-                            <div class="col-md-3">
-                                
-                            </div>
+                          
 
                         </div>
 
+                        
 
-                        <div v-for="user in userList" :key="user.id" class="d-flex align-items-center justify-content-between mt-5 mb-5" >
-                            <!-- <img :src="user.image" alt="image here" style="margin-top: 5%;"> -->
-                            <div class="user-avatar">
-                                <img :src="user.image" alt="image here">
-                            </div>
-                            <h5 style="margin-top: 5%;">{{ user.first_name }}</h5>
-                            <div v-if="user.verified === 'Yes'" class="d-flex align-items-center">
-                                <img class="img2" src="@/assets/verified.svg" alt="Verified Icon">
-                                <h6 class="mt-2">Verified</h6>
-                            </div>
-                            <button class="btn  btn-small text-light" style=" width: 120px; margin-top: 5%; background-color: #D6A12B">View All</button>
-                        </div>
+                        
                     </div>
-                   
+                    
+                
+
+
+                  
+
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li v-bind:class="[{ disabled: !pagination.prev_page_url }]" class="page-item">
+                            <a class="page-link" href="#" @click.prevent="fetchUserList(pagination.prev_page_url)">Previous</a>
+                            </li>
+
+                            <li class="page-item disabled">
+                            <a class="page-link text-dark" href="#">Page {{ pagination.current_page }} of {{ pagination.last_page }}</a>
+                            </li>
+
+                            <li v-bind:class="[{ disabled: !pagination.next_page_url }]" class="page-item">
+                            <a class="page-link" href="#" @click.prevent="fetchUserList(pagination.next_page_url)">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+
                 </div>
 
          
 
-
-    
+         
+                
 
     </div>
 </template>
@@ -123,7 +138,8 @@ export default {
                 { label: 'Option 3', value: 'option3' }
             ],
 
-            userList: []
+            userList: [],
+            pagination: {},
         };
     },
 
@@ -140,11 +156,12 @@ export default {
             this.showDropdown = false;
         },
 
-        async fetchUserList(){
+        async fetchUserList(page_url){
             try {
 
                 const token = localStorage.getItem('adminlogin');
-                const res = await fetch('https://stagingapp2.fintabng.com/api/v1/admin/users',{
+                page_url = page_url || 'https://stagingapp2.fintabng.com/api/v1/admin/users';
+                const res = await fetch(page_url ,{
                     method: "GET",
                     headers: {
                         "Accept": "application/json",
@@ -156,13 +173,26 @@ export default {
                     throw new Error('Network was Not ok');
                 }
                 const data = await res.json();
-                // console.log(data.data);
+                console.log(data.links);
                 this.userList = data.data;
-                
+                this.makePagination(data.meta, data.links);
+
             } catch (error) {
                 console.log('There was a pronlem fetching the userlist:',error.message);
             }
-        }
+        },
+
+
+        makePagination(meta, links) {
+            let pagination = {
+                current_page: meta.current_page,
+                last_page: meta.last_page,
+                next_page_url: links.next,
+                prev_page_url: links.prev
+            };
+
+            this.pagination = pagination;
+        },
 
     }
 };
